d97a153d79f289e08dc6c981f154a22d39d98256
vidigummy <vidigummy@gmail.com>
2022-02-24 14:29:46 +0900

    :tada:

---
+++ src/main/java/programers/stack_queue/Truck.java
@@ -0,0 +1,11 @@
+package programers.stack_queue;
+
+public class Truck {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args){
+        
+    }
+}
