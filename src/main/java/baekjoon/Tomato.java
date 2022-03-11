2d3c63081239ca9e8f46d6c596916d4adf0403b6
vidigummy <vidigummy@gmail.com>
2022-03-11 22:22:36 +0900

    tomatoes

--- src/main/java/baekjoon/Tomato.java
+++ src/main/java/baekjoon/Tomato.java
@@ -3,33 +3,52 @@ package baekjoon;
 import java.util.*;
 
 public class Tomato {
-    static HashMap<Integer[], Set<Integer[]>> total_graph = new HashMap<>();
-    static HashMap<Integer[], Integer> now_state = new HashMap<>();
-    static List<Integer[]> doneTomato = new ArrayList<>();
-
+    static int[][] tomatoes;
+    static int[] dx = {-1,1,0,0};//상하좌우
+    static int[] dy = {0,0,-1,1}; //상하좌우
+    static int cnt = 0;
+    static int depth = 0;
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n,m;
-        m = sc.nextInt();
-        n = sc.nextInt();
-        for(int y =0 ; y < n; y++){
+        m = sc.nextInt();//가로
+        n = sc.nextInt();//세로
+        Queue<int[]> queue = new LinkedList<>();
+        tomatoes = new int[n][m];
+        for(int y = 0; y < n; y++){//입력
             for(int x = 0; x < m; x++){
-                int now = sc.nextInt();
-                Integer[] now_xy = {x, y};
-                now_state.put(now_xy, now);
-                if(now == 1){
-                    doneTomato.add(now_xy);
+                tomatoes[y][x] = sc.nextInt();
+                if(tomatoes[y][x] == 1){
+                    queue.add(new int[] {y,x});
                 }
-//                if(y == 0){
-//                    if(x == 0){
-//
-//                    }
-//                }else{
-//
-//                }
+                else if(tomatoes[y][x] ==0){
+                    cnt++;
+                }
             }
         }
-        System.out.println(now_state);
+
+        while (cnt >0 && !queue.isEmpty()){
+            for(int i = queue.size(); i > 0; i--){
+                int[] tmp = queue.poll();
+                for(int j = 0; j < 4; j++){
+                    int nx = tmp[1] + dx[j];
+                    int ny = tmp[0] +dy[j];
+
+                    if (ny < 0 || nx < 0 || ny >= n || nx >= m || tomatoes[ny][nx] != 0){
+                        continue;
+                    }
+                    cnt--;
+                    tomatoes[ny][nx] = 1;
+                    queue.add(new int[] {ny, nx});
+                }
+            }
+            depth++;
+        }
+        if(cnt == 0){
+            System.out.println(depth);
+        }else {
+            System.out.println(-1);
+        }
     }
 
 }
